6e1025249f7c9931b302a2f10adb6f5681024981
Jonathan Bairstow <jonathanbairstow@hotmail.com>
2019-05-30 11:31:30 +0100

    Created as Stored Proc

--- SSIS/Execution Summary.sql
+++ SSIS/Execution Summary.sql
@@ -1,36 +1,42 @@
+Create Procedure dbo.usp_ExecutionSummary
+as
 
-SELECT	[folder_name]
-		,[project_name]
-		,[package_name]
-		,[environment_name] 
-		,CASE [status] 
-			WHEN 1 THEN 'created' 
-			WHEN 2 THEN 'Running' 
-			WHEN 3 THEN 'canceled' 
-			WHEN 4 THEN 'failed' 
-			WHEN 5 THEN 'pending' 
-			WHEN 6 THEN 'ended unexpectedly' 
-			WHEN 7 THEN 'succeeded' 
-			WHEN 8 THEN 'stopping' 
-			WHEN 9 THEN 'completed' 
-		END ExecutionStatus
-		,count(*) Executions
-		,cast(min([start_time]) as smalldatetime) EarliestExecution
-		,cast(max([start_time]) as smalldatetime) LatestExecution
-FROM	[SSISDB].[catalog].[executions]
-where	folder_name = 'Logistics'
-group by [folder_name]
-		,[project_name]
-		,[package_name]
-		,[environment_name] 
-		,CASE [status] 
-			WHEN 1 THEN 'created' 
-			WHEN 2 THEN 'Running' 
-			WHEN 3 THEN 'canceled' 
-			WHEN 4 THEN 'failed' 
-			WHEN 5 THEN 'pending' 
-			WHEN 6 THEN 'ended unexpectedly' 
-			WHEN 7 THEN 'succeeded' 
-			WHEN 8 THEN 'stopping' 
-			WHEN 9 THEN 'completed' 
-		END
+	SELECT	[folder_name]
+			,[project_name]
+			,[package_name]
+			,[environment_name] 
+			,CASE [status] 
+				WHEN 1 THEN 'created' 
+				WHEN 2 THEN 'Running' 
+				WHEN 3 THEN 'canceled' 
+				WHEN 4 THEN 'failed' 
+				WHEN 5 THEN 'pending' 
+				WHEN 6 THEN 'ended unexpectedly' 
+				WHEN 7 THEN 'succeeded' 
+				WHEN 8 THEN 'stopping' 
+				WHEN 9 THEN 'completed' 
+			END ExecutionStatus
+			,count(*) Executions
+			,cast(min([start_time]) as smalldatetime) EarliestExecution
+			,cast(max([start_time]) as smalldatetime) LatestExecution
+	FROM	[SSISDB].[catalog].[executions]
+	group by [folder_name]
+			,[project_name]
+			,[package_name]
+			,[environment_name] 
+			,CASE [status] 
+				WHEN 1 THEN 'created' 
+				WHEN 2 THEN 'Running' 
+				WHEN 3 THEN 'canceled' 
+				WHEN 4 THEN 'failed' 
+				WHEN 5 THEN 'pending' 
+				WHEN 6 THEN 'ended unexpectedly' 
+				WHEN 7 THEN 'succeeded' 
+				WHEN 8 THEN 'stopping' 
+				WHEN 9 THEN 'completed' 
+			END
+	order by [folder_name]
+			,[project_name]
+			,[package_name]
+			,[environment_name]
+			, ExecutionStatus
